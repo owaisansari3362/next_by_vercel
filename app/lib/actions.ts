71a3d5006538a422f9547e25bb4136e9ca19bcc2
owaisansari <muhammad.owais@squatwolf.com>
2024-12-10 12:04:26 +0500

    Checking if returning error messages from action working

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -92,14 +92,19 @@ export async function updateInvoice(id: string, formData: FormData) {
         `;
     } catch (e) {
 
-        // return {message: 'Database Error: Failed to Update Invoice.'};
+        return {message: 'Database Error: Failed to Update Invoice.'};
     }
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
-    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    try {
+        await sql`DELETE FROM invoices WHERE id = ${id}`;
+    } catch (e) {
+        return {message: 'Database Error: Failed to delete Invoice.'};
+    }
+
     revalidatePath('/dashboard/invoices');
 }
 
